fd2c62ae5760e163f083b35b731d7f4ecf1daf9b
Ahmet Ozan Tatlısu <ahmetozan789@gmail.com>
2018-05-25 17:00:05 +0300

    Add files via upload

---
+++ UI/ChairControl/app/src/main/java/iot/chaircontrol/Entry.java
@@ -0,0 +1,27 @@
+package iot.chaircontrol;
+
+public class Entry {
+    private String time;
+    private int value;
+
+    public Entry(String time, int value) {
+        this.time = time;
+        this.value = value;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
